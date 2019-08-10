7d61496f10333570d123995ab83b0952059ed047
koin <kangyueluo777@gmail.com>
2019-08-10 09:22:41 +0800

    Initial commit

---
+++ test_driver/app.dart
@@ -0,0 +1,19 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter_driver/driver_extension.dart';
+import 'package:flutter_app44/main.dart' as app;
+
+
+void main() {
+  // This line enables the extension.
+  enableFlutterDriverExtension();
+
+  // Call the `main()` function of the app, or call `runApp` with
+  // any widget you are interested in testing.
+  app.main();
+}
